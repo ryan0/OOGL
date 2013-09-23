647e4d522e1198f9a78bd3d959e2bdfe7756d809
Ryan Miller <ryan.miller4224@gmail.com>
2013-09-23 19:43:59 -0400

    put shaders in separate file

--- src/OOGLshader.cpp
+++ src/OOGLshader.cpp
@@ -1,28 +1,7 @@
 #include "OOGLshader.h"
+#include "shaders.h"
 #include <iostream>
 
-
-const char* NormalVertexShader = 
-	"#version 330 \n"
-	"layout(location = 0) in vec2 point;"
-	"layout(location = 1) in vec2 pointUV;"
-	"out vec2 UV;"
-	"void main()"
-	"{"
-	"	gl_Position = vec4(point, 0, 1);"
-	"	UV = pointUV;"
-	"}";
-
-
-const char* NormalFragmentShader = 
-	"#version 330 \n"
-	"in vec2 UV;"
-	"out vec4 color;"
-	"uniform sampler2D sampler;"
-	"void main()"
-	"{"
-	"	color = texture(sampler, UV).rgba;"
-	"}";
 
 namespace oogl
 {
