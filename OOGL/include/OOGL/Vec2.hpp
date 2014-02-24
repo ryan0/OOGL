1f87728ba39e4716f1645244f523282182206b2b
Ryan <ryan.miller4224@gmail.com>
2014-02-24 05:26:21 -0500

    cleaned code

--- OOGL/include/OOGL/Vec2.hpp
+++ OOGL/include/OOGL/Vec2.hpp
@@ -11,11 +11,11 @@ namespace gl
 	struct Vec2
 	{	
 		T x,y;
-
 		Vec2()					: x(0), y(0) {}
 		Vec2(T Z)				: x(Z), y(Z) {}
 		Vec2(T X, T Y)			: x(X), y(Y) {}
 		Vec2(const Vec2& vec2)	: x(vec2.x), y(vec2.y) {}
+
 
 		Vec2& operator=(const Vec2& vec2)
 		{
